49b7389ed750b7f9735c26ae19547e6a5bc0aa24
guerrand <w.guerrand@lancaster.ac.uk>
2020-03-20 15:58:40 +0100

    Initial commit

---
+++ main.py
@@ -0,0 +1,24 @@
+# import sys, pygame
+# pygame.init()
+#
+# size = width, height = 1000, 500
+# speed = [2, 2]
+# black = 0, 0, 0
+# clockobject = pygame.time.Clock()
+#
+# screen = pygame.display.set_mode(size)
+#
+# ball = pygame.image.load("intro_ball.png")
+# ballrect = ball.get_rect()
+#
+#
+# while 1:
+#     for event in pygame.event.get():
+#         if event.type == pygame.QUIT: sys.exit()
+#
+#
+#
+#
+#     screen.fill(black)
+#     screen.blit(ball, ballrect)
+#     pygame.display.flip()
