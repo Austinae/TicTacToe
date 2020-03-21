e26e5bcfa4954e899b94a47efa1df2cee8cd40c5
guerrand <w.guerrand@lancaster.ac.uk>
2020-03-21 17:02:40 +0100

    Code is cleaner

--- main.py
+++ main.py
@@ -8,6 +8,8 @@ black = 0, 0, 0
 
 screen = pygame.display.set_mode(size)
 
+gameRunning = True
+
 circleSize = 60
 thickness = 5
 
@@ -25,8 +27,14 @@ crosses = []
 player1 = "circle"
 player2 = "cross"
 
+player1score = 0
+player2score = 0
+
 player = player1
 
+# label to show score
+font = pygame.font.Font(None, 30)
+scoretext = font.render("P1 (O):" + str(player1score)+"   P2 (X):" + str(player2score), 1, black)
 
 class Circle:
     def __init__(self, surface, color, center, radius, width):
@@ -110,6 +118,11 @@ class Board:
 
 
 def drawObjects(circles, crosses):
+    # this is the board
+    pygame.draw.rect(screen, black, (thirdwidth, 0, thickness, height))
+    pygame.draw.rect(screen, black, (thirdwidth*2, 0, thickness, height))
+    pygame.draw.rect(screen, black, (0, thirdheight, width, thickness))
+    pygame.draw.rect(screen, black, (0, thirdheight*2, width, thickness))
     for circle in circles:
         circle.draw()
     for cross in crosses:
@@ -117,58 +130,66 @@ def drawObjects(circles, crosses):
 
 board = Board()
 
-while 1:
+def buttonDownLogic(board, player, circles, crosses, player1score, player2score, scoretext):
+    x, y = pygame.mouse.get_pos()
+    w = 0
+    z = 0
+
+    if x // int(thirdwidth) == 0:
+        w = 0
+        x = 1
+
+    else:
+        w = x // int(thirdwidth)
+        x = (x // int(thirdwidth)) * 2 + 1
+
+    if y // int(thirdheight) == 0:
+        z = 0
+        y = 1
+
+    else:
+        z = y // int(thirdheight)
+        y = (y // int(thirdheight)) * 2 + 1
+
+    if board.isMoveValid(w, z):
+        if player == player1:
+            circles.append(Circle(screen, black, (centerx * x, centery * y), circleSize, thickness))
+        else:
+            crosses.append(
+                Cross(screen, black, w * int(width / 3) + cross_offset, z * int(height / 3) + cross_offset, thickness))
+        board.move(w, z, player)
+        print(board.board)
+        if board.isWinner():
+            print(player, "is the winner")
+            if player == player1:
+                player1score += 1
+            else:
+                player2score += 1
+            scoretext = font.render("P1 (O):" + str(player1score) + "   P2 (X):" + str(player2score), 1, black)
+            board.empty()
+            circles = []
+            crosses = []
+        elif None not in board.board.values():
+            print("It is a draw")
+            board.empty()
+            circles = []
+            crosses = []
+        if player == player1:
+            player = player2
+        else:
+            player = player1
+    return board, player, circles, crosses, player1score, player2score, scoretext
+
+while gameRunning:
     for event in pygame.event.get():
         if event.type == pygame.QUIT: sys.exit()
 
         if event.type == pygame.MOUSEBUTTONDOWN:
-            x, y = pygame.mouse.get_pos()
-            w = 0
-            z = 0
-
-            if x // int(thirdwidth) == 0:
-                w = 0
-                x = 1
-
-            else:
-                w = x // int(thirdwidth)
-                x = (x // int(thirdwidth)) * 2 + 1
-
-
-            if y // int(thirdheight) == 0:
-                z = 0
-                y = 1
-
-            else:
-                z = y // int(thirdheight)
-                y = (y // int(thirdheight)) * 2 + 1
-
-
-
-            if board.isMoveValid(w, z):
-                if player == player1:
-                    circles.append(Circle(screen, black, (centerx * x, centery * y), circleSize, thickness))
-                else:
-                    crosses.append(Cross(screen, black, w * int(width/3) + cross_offset, z * int(height/3) + cross_offset, thickness))
-                board.move(w, z, player)
-                if board.isWinner():
-                    print(player, "is the winner")
-                    board.empty()
-                    circles = []
-                    crosses = []
-                if player == player1:
-                    player = player2
-                else:
-                    player = player1
-
+            board, player, circles, crosses, player1score, player2score, scoretext = buttonDownLogic(board, player, circles, crosses, player1score, player2score, scoretext)
 
     screen.fill(white)
-    pygame.draw.rect(screen, black, (thirdwidth, 0, thickness, height))
-    pygame.draw.rect(screen, black, (thirdwidth*2, 0, thickness, height))
-    pygame.draw.rect(screen, black, (0, thirdheight, width, thickness))
-    pygame.draw.rect(screen, black, (0, thirdheight*2, width, thickness))
-
-
     drawObjects(circles, crosses)
+    screen.blit(scoretext, (0, 0))
+
 
     pygame.display.flip()
